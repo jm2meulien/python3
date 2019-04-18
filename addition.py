8393025cb3edff584e1e324782dec6798e2abdeb
jm2meulien <jm2meulien@hotmail.fr>
2019-04-18 17:43:14 +0200

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,10 @@
+# Ce programme additionne deux nombres
+
+num1 = 1.5
+num2 = 6.3
+
+# additionne deux nombres
+sum = float(num1) + float(num2)
+
+# Affiche la somme
+print('La somme de {0} et {1} est {2}'.format(num1, num2, sum))
