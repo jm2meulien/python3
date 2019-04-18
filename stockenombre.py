95ab67fcac1fc3e7642866898c1367b4592523c9
jm2meulien <jm2meulien@hotmail.fr>
2019-04-18 17:44:05 +0200

    Create stockenombre.py

---
+++ stockenombre.py
@@ -0,0 +1,9 @@
+# Stocke les nombres en entrée
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# additionne deux nombres
+sum = float(num1) + float(num2)
+
+# Affiche la somme
+print('La somme de {0} et {1} est {2}'.format(num1, num2, sum))
